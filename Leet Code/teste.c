1bac5b540db57f2ae8608e240762b30225ba8797
gabrielcristhie <gcristhie@hotmail.com>
2023-09-05 14:40:34 -0300

    Add exercicies

---
+++ Leet Code/teste.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+	int a, b ;
+	int soma;
+	printf("Insira sua variavel A:");
+	scanf("%d",&a);
+	printf("Insira sua variavel B:");
+	scanf("%d",&b);
+	soma=a+b;
+
+	printf("Tama : %d",soma);
+
+
+
+return (0);
+}
